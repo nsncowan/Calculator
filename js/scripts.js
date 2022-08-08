e39e4c9fba486a44d2fdbf2e8937af489878c5b2
Grace Lee <gracexsnoopy@gmail.com>
2022-08-08 14:58:18 -0700

    combine alerts into one
    Co-authored-by: Noah Cowan <nsncowan@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -19,10 +19,9 @@ function divide(number1, number2) {
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-window.alert("the addition of your numbers is " + (add(number1, number2)).toString());
+const alert1 = "the addition of your numbers is " + (add(number1, number2)).toString();
+const alert2 = "the subtraction of your numbers is " + (subtract(number1, number2)).toString();
+const alert3 = "the multiplication of your numbers is " + (multiply(number1, number2)).toString();
+const alert4 = "the division of your numbers is " + (divide(number1, number2)).toString();
 
-window.alert("the subtraction of your numbers is " + (subtract(number1, number2)).toString());
-
-window.alert("the multiplication of your numbers is " + (multiply(number1, number2)).toString());
-
-window.alert("the division of your numbers is " + (divide(number1, number2)).toString());
+window.alert(alert1 + " ;" + alert2 + " ;" + alert3 + " ;"+ alert4 + " ;");
