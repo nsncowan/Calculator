2ceb1666ba8e31f4e3885b19da4fd167a2084fb0
Grace Lee <gracexsnoopy@gmail.com>
2022-08-08 14:44:52 -0700

    add 3 more arithmetic functions and related alerts
    Co-authored-by: Noah Cowan <nsncowan@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,26 @@ function add(number1, number2) {
   return number1 + number2
 }
 
+function subtract(number1, number2) {
+  return number1 - number2
+}
+
+function multiply(number1, number2) {
+  return number1 * number2
+}
+
+function divide(number1, number2) {
+  return number1 / number2
+}
+
 // user interface logic
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-window.alert(add(number1, number2));
+window.alert("the addition of your numbers is " + (add(number1, number2)).toString());
+
+window.alert("the subtraction of your numbers is " + (subtract(number1, number2)).toString());
+
+window.alert("the multiplication of your numbers is " + (multiply(number1, number2)).toString());
+
+window.alert("the division of your numbers is " + (divide(number1, number2)).toString());
